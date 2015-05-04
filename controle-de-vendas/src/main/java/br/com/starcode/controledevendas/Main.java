c7382a12c811d6d684a475653bd527ea5c9a8631
boca04 <lucasfernandobocanegra@gmail.com>
2015-05-04 15:29:45 -0300

    criacao do projeto do maven do projeto e comeco da implementacao das classes da logica

---
+++ controle-de-vendas/src/main/java/br/com/starcode/controledevendas/Main.java
@@ -0,0 +1,10 @@
+package br.com.starcode.controledevendas;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+	public static void main(String args[])
+	{
+		JOptionPane.showMessageDialog(null,"TESTE MAVEN");
+	}
+}
