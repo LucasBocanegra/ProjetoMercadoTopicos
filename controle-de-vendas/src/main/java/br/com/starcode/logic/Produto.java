1014eb9e067470f4ead17a4fbdbfb671fde37048
boca04 <lucasfernandobocanegra@gmail.com>
2015-05-28 00:54:35 -0300

    implementacao das classes produto, cliente e registroVenda

--- controle-de-vendas/src/main/java/br/com/starcode/logic/Produto.java
+++ controle-de-vendas/src/main/java/br/com/starcode/logic/Produto.java
@@ -15,21 +15,124 @@ public class Produto {
 	private double estoque;
 	private int status;
 	
-	Produto(int codigo,String descricao, double precoCompra, double precoVenda, String unidade,
-			double estoque, int status){
-		this.codigo = codigo;
-		this.descricao = descricao;
-		this.precoCompra = precoCompra;
-		this.precoVenda = precoVenda;
-		this.unidade = unidade;
-		this.estoque = estoque;
-		this.status = status;		
+	public Produto(int codigo,String descricao, double precoCompra, double precoVenda, String unidade,
+			double estoque, int status) throws RuntimeException{	
+		
+		setCodigo(codigo);
+		setDescricao(descricao);
+		setPrecoCompra(precoCompra);
+		setPrecoVenda(precoVenda);
+		setUnidade(unidade);
+		setEstoque(estoque);
+		setStatus(status);				
 	}
 	
 	public void alteraProduto(int codigo, String descricao, double precoCompra, double precoVenda,
 			String unidade, double estoque, int status){
 		
+		setCodigo(codigo);
+		setDescricao(descricao);
+		setPrecoCompra(precoCompra);
+		setPrecoVenda(precoVenda);
+		setUnidade(unidade);
+		setEstoque(estoque);
+		setStatus(status);		
 	}
 	
-	public void estoque(int codigo, double estoque){}
+	//obs: não seria necessário fornecer o código do produto
+	public void alteraEstoque(int codigo, double estoque)throws RuntimeException{
+		if(codigo >= 0 && this.codigo == codigo && estoque >= 0)
+			this.estoque = estoque;
+		else
+			throw new RuntimeException();
+	}
+	
+	public void alteraEstoque(double estoque)throws RuntimeException{
+		if(estoque >= 0)
+			this.estoque = estoque;
+		else
+			throw new RuntimeException();
+	}
+		
+
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	//codigo nao pode ser repetido
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) throws RuntimeException {
+		if(descricao.length() >= 3 && descricao.length() <= 64)
+			this.descricao = descricao;
+		else
+			throw new RuntimeException();
+	}
+
+	public double getPrecoCompra() {
+		return precoCompra;
+	}
+
+	public void setPrecoCompra(double precoCompra) throws RuntimeException{
+		if(precoCompra >= 0)
+			this.precoCompra = precoCompra;
+		else
+			throw new RuntimeException();
+	}	
+
+	public double getPrecoVenda() {
+		return precoVenda;
+	}
+
+	public void setPrecoVenda(double precoVenda) throws RuntimeException{
+		if(precoVenda >= this.precoCompra)
+			this.precoVenda = precoVenda;
+		else
+			throw new RuntimeException();
+	}
+
+	public String getUnidade() {
+		return unidade;
+	}
+
+	public void setUnidade(String unidade) throws RuntimeException{
+		if(unidade.length() == 2)
+			this.unidade = unidade;
+		else
+			throw new RuntimeException();
+	}
+
+	public double getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(double estoque) throws RuntimeException{
+		if(estoque >= 0)
+			this.estoque = estoque;
+		else
+			throw new RuntimeException();
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) throws RuntimeException {
+		if(status == 1 || status == 0){
+			if(status == 0 && estoque >= 0)
+				throw new RuntimeException();
+			else
+				this.status = status;
+		}
+		else
+			throw new RuntimeException();
+			
+	}	
+	
 }
